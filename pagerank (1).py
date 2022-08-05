7ece34deae7d639cc67c6cbcc86a27cfd8b1f302
vatsalshah2002 <74639313+vatsalshah2002@users.noreply.github.com>
2022-08-05 21:52:04 +0530

    Add files via upload

---
+++ pagerank (1).py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def pagerank(M, num_iterations: int = 4500, d: float = 0.85):
+    """PageRank: The trillion dollar algorithm."""
+    N = M.shape[1]
+    v = np.random.rand(N, 1)
+    v = v / np.linalg.norm(v, 1)
+    M_hat = (d * M + (1 - d) / N)
+    for i in range(num_iterations):
+        v = M_hat @ v
+    return v
+
+              
+n = int(input("Enter the number of rows:"))
+m = int(input("Enter the number of columns:"))
+print("Enter the entries rowise (separated by space): ")
+matrix=[]
+for i in range(n):
+    a=[]
+    for j in range(m):
+        a.append(float(input()))
+    matrix.append(a)
+
+M=np.array(matrix)
+
+# v = pagerank(M, 100, 0.86)
+v_absolute=pagerank(M, 100, 1)
+print("Pagerank of given Network: ")
+print(v_absolute)
